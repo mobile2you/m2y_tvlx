8f43a7a0525ac59af762909af17c30fe79c577d5
Rodrigo_Mobile <rodrigo.almeida@mobile2you.com.br>
2022-11-30 16:08:25 -0300

    feat(TRAV): (create_key)

--- lib/m2y_tvlx/modules/tvlx_pix_keys.rb
+++ lib/m2y_tvlx/modules/tvlx_pix_keys.rb
@@ -1,64 +1,56 @@
 module M2yTvlx
-
   class TvlxPixKeys < TvlxModule
-
-    def initialize(client_id, client_secret, url, scope, auth_url)
-      @auth = TvlxAuth.new(client_id, client_secret, url)
+    def initialize(client_id, client_secret, url)
+      @auth = TvlxAuth.new(client_id, client_secret, url + PIX_AUTH_PATH)
       @client_id = client_id
       @client_secret = client_secret
       @url = url
-      @auth = TvlxAuth.new(client_id, client_secret, auth_url + PIX_AUTH_PATH, scope)
     end
 
     def refreshToken
-      if TvlxHelper.shouldRefreshToken?(@client_secret)
-        @auth.generateToken
-      end
+      @auth.generateToken if TvlxHelper.shouldRefreshToken?(@client_secret)
     end
-
 
     def list_keys(body)
       refreshToken
       url = @url + PIX_LIST_KEYS_PATH
       headers = json_headers
-      headers["Authorization"] = "Bearer #{TvlxHelper.get_token(@client_secret)}"
-      req = HTTParty.post(url, body: body.to_json, :verify => false, headers: headers)
+      headers['Authorization'] = "Bearer #{TvlxHelper.get_token(@client_secret)}"
+      req = HTTParty.post(url, body: body.to_json, verify: false, headers: headers)
       begin
         TvlxModel.new(req.parsed_response)
-      rescue
+      rescue StandardError
         nil
       end
     end
 
-    def generate_key(body)
+    def create_key(body)
       refreshToken
-      url = @url + PIX_ADD_KEY_PATH
+      url = @url + PIX_CREATE_KEY_PATH
       headers = json_headers
-      headers["Authorization"] = "Bearer #{TvlxHelper.get_token(@client_secret)}"
-      headers["Chave-Idempotencia"] = SecureRandom.uuid
-      req = HTTParty.post(url, body: body.to_json, :verify => false, headers: headers)
+      headers['Authorization'] = "Bearer #{TvlxHelper.get_token(@client_secret)}"
+      headers['Chave-Idempotencia'] = SecureRandom.uuid
+      req = HTTParty.post(url, body: body.to_json, verify: false, headers: headers)
       begin
         TvlxModel.new(req.parsed_response)
-      rescue
+      rescue StandardError
         nil
       end
     end
 
     def remove_key(id, body)
       refreshToken
-      url = @url + PIX_REMOVE_KEY_PATH + "#{id}"
+      url = @url + PIX_REMOVE_KEY_PATH + id.to_s
       puts url
       headers = json_headers
-      headers["Authorization"] = "Bearer #{TvlxHelper.get_token(@client_secret)}"
-      headers["Chave-Idempotencia"] = SecureRandom.uuid
-      req = HTTParty.post(url, body: body.to_json, :verify => false, headers: headers)
+      headers['Authorization'] = "Bearer #{TvlxHelper.get_token(@client_secret)}"
+      headers['Chave-Idempotencia'] = SecureRandom.uuid
+      req = HTTParty.post(url, body: body.to_json, verify: false, headers: headers)
       begin
         TvlxModel.new(req.parsed_response)
-      rescue
+      rescue StandardError
         nil
       end
     end
-
   end
-
 end
