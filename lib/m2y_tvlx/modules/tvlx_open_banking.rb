38d3bbda9783024a0801da235d1425cf18a82c7d
Breno Cardoso <elisio.cardoso@dimensa.com.br>
2023-02-17 23:32:34 -0300

    refactor(TRAV-1339): Getting Open Banking Authorization code

--- lib/m2y_tvlx/modules/tvlx_open_banking.rb
+++ lib/m2y_tvlx/modules/tvlx_open_banking.rb
@@ -1,37 +1,33 @@
 module M2yTvlx
-  class TvlxOpenBanking < TvlxModule
+  class TvlxOpenBanking
     def initialize(client_id, redirect_uri, url, state, auth)
-      @auth = banking_auth(client_id, redirect_uri, url, state, auth)
       @client_id = client_id
-      @client_secret = client_secret
+      @redirect_uri = redirect_uri
       @url = url
+      @state = state
+      @authenticator = auth
+
+      @authorization_code = authorization_code
     end
 
-    def banking_auth(client_id, redirect_uri, url, state, auth)
-      grant_code = grant_code(url, client_id, redirect_uri, state)
-      authorization_code(url, auth, grant_code)
+    def authorization_code
+      authorization_code_url = "#{@url}#{BANKING_AUTH_PATH}"
+      headers = {
+        'Content-Type' => 'application/x-www-form-urlencoded',
+        'Authorization' => "Basic #{@authenticator}"
+      }
+      body = URI.encode_www_form({
+        grant_type: 'authorization_code', 
+        code: grant_code
+      })
+      response = HTTParty.post(authorization_code_url, body: body, headers: headers)
+      response['access_token'].present? ? response['access_token'] : ''
     end
 
-    def grant_code(url, client_id, redirect_uri, state)
-      grant_code_url = "#{url}#{GRANT_CODE_PATH}&client_id=#{client_id}&redirect_uri=#{redirect_uri}&state=#{state}"
-      response = HTTParty.post(grant_code_url,
-                                 headers: {
-                                   'WWW-callback' => '0'
-                                 })
-      param = response.parsed_response['redirect_uri'].split('=')[2]
-      param.parsed_response['redirect_uri'].split('&')[0]
-    end
-
-    def authorization_code(url, auth, grant_code)
-      body = {grant_type: authorization_code, code: grant_code}
-      authorization_code_url = "#{url}#{BANKING_AUTH_PATH}"
-      authorization_code_resp = HTTParty.post(authorization_code_url,
-                                 body: body.to_json,
-                                 headers: {
-                                  'Content-Type' => 'application/x-www-form-urlencoded'
-                                  'Authorization' => "Bearer #{auth}"
-                                 })
-      authorization_code_resp.parsed_response['access_token']
+    def grant_code
+      grant_code_url = "#{@url}#{GRANT_CODE_PATH}&client_id=#{@client_id}&redirect_uri=#{@redirect_uri}&state=#{@state}"
+      response = HTTParty.get(grant_code_url, headers: { 'callback' => '0' })
+      response['redirect_uri'].present? ? response['redirect_uri'].split('=')[2] : ''
     end
   end
 end
